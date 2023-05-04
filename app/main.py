f91a53fe857917ad0b0a8c45e56e9894acfb720d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 11:38:56 +0300

    Defined my first websocket endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 """This is the entrypoint to the project"""
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
 
 
 app = FastAPI()
@@ -14,3 +15,20 @@ app = FastAPI()
 async def home() -> dict[str, str]:
     """This is the home endpoint"""
     return {"message": "Hello World"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket) -> None:
+    """A simple websocket endpoint
+
+    Args:
+        websocket (WebSocket): The websocket object
+    """
+    await websocket.accept()
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message received: {data}")
+    except WebSocketDisconnect:
+        await websocket.close()
