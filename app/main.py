dcd65f90194bfdd4c428a641cf7f46fa90594f1c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 12:10:28 +0300

    Refactored the python script

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 """This is the entrypoint to the project"""
+import asyncio
+from datetime import datetime
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 
@@ -17,6 +19,26 @@ async def home() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
+async def echo_message(websocket: WebSocket):
+    """This os a function that shows the message
+
+    Args:
+        websocket (WebSocket): The websocket object
+    """
+    data = await websocket.receive_text()
+    await websocket.send_text(f"Message text was: {data}")
+
+
+async def send_time(websocket: WebSocket):
+    """Defined a function that will show the send time of messages
+
+    Args:
+        websocket (WebSocket): The websocket object
+    """
+    await asyncio.sleep(10)
+    await websocket.send_text(f"It is: {datetime.utcnow().isoformat()}")
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket) -> None:
     """A simple websocket endpoint
@@ -32,13 +54,3 @@ async def websocket_endpoint(websocket: WebSocket) -> None:
             await websocket.send_text(f"Message received: {data}")
     except WebSocketDisconnect:
         await websocket.close()
-
-
-async def echo_message(websocket: WebSocket):
-    """This os a function that shows the message
-
-    Args:
-        websocket (WebSocket): The websocket object
-    """
-    data = await websocket.receive_text()
-    await websocket.send_text(f"Message text was: {data}")
