0f6cb36af8883a817fb4f1cf47d05b70c0864c73
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 12:24:36 +0300

    Imported the necessary libraries

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter, WebSocket
+
+
+dependencies = APIRouter(prefix="/dependencies", tags=["P2P communication"])
+
 """A python script for the dependencies"""
+@app.websocket("/ws")
+async def websocket_another_endpoint(websocket: WebSocket):
