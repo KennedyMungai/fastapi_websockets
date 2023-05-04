2bf156bc0e80c1500a9a6eef86e2b3f228d0a5e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-04 11:11:59 +0300

    Created the basic endpoint for the application

---
+++ app/main.py
@@ -0,0 +1,11 @@
+"""This is the entrypoint to the project"""
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/", name="home", description="This is the root endpoint for the application", tags=["Home"])
+async def home() -> dict[str, str]:
+    """This is the home endpoint"""
+    return {"message": "Hello World"}
